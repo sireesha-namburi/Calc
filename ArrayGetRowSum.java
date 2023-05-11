0151bb7f2a6940f5fef8f42e1d0a8c2baed5ecc4
Sireesha Namburu <sn23023@corp.ojas-it.com>
2023-05-11 09:47:27 +0530

    first commit

---
+++ ArrayGetRowSum.java
@@ -0,0 +1,34 @@
+package topcrowd;
+
+import java.util.Arrays;
+
+public class ArrayGetRowSum {
+	
+
+	static int[] getRowSum(int arr[][]) {
+
+		int i, j, sum = 0;
+		int m = 3, n = 3;
+		int op[] = new int[3];
+		
+		for (i = 0; i < m; ++i) {
+			for (j = 0; j < n; ++j)
+				sum = sum + arr[i][j];
+
+			op[i] = sum;
+			sum = 0;
+
+		}
+		
+		return op;
+	}
+
+	public static void main(String[] args) {
+		int input[][] = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+		int outp[];
+		outp = getRowSum(input);
+		System.out.println(Arrays.toString(outp));
+
+	}
+}
+
